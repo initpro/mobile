d8105144ce49be2d66784c7cbdc6b367eedb1236
Misha <stupnikov@Debian-60-squeeze-64-minimal>
2014-06-11 17:16:47 +0600

    welcome add table

--- app/view/Wellcome.js
+++ app/view/Wellcome.js
@@ -25,7 +25,7 @@ Ext.define('InitProMobile.view.Wellcome', {
                     }
                 },
 
-                itemTpl: '<div style="padding: 30px;"><div style="text-align:center;margin-bottom:20px;"><img src="http://initpro.ru/assets/base/images/logo/logo_0.png" /></div><div style="text-align:justify;">Добро пожаловать на сайт информационно аналитической системы InitPro. Здесь вы найдете полную информацию обо всех коммерческих тендерах , а также государственных закупках Российской Федерации, и всегда сможете скачать всю необходимую для участия в них документацию.</div><div><table width="100%" style="margin-top:20px; text-align:center;"><tr><td colspan="2">Количество тендеров</td><tr><tr><td width="50%">Всего</td><td width="50%">Планируемые</td></tr><tr><td>{allCount}</td><td>{plannedCount}</td></tr></table></div></div>'
+                itemTpl: '<div class="wellcome"><div class="image"><img src="/assets/base/images/logo/logo_0.png" /></div><div class="title">Добро пожаловать на сайт информационно аналитической системы InitPro. Здесь вы найдете полную информацию обо всех коммерческих тендерах , а также государственных закупках Российской Федерации, и всегда сможете скачать всю необходимую для участия в них документацию.</div><div><table width="100%" class="table_1"><tr><td colspan="2">Количество тендеров</td><tr><tr><td width="50%">Всего</td><td width="50%">Планируемые</td></tr><tr><td>{allCount}</td><td>{plannedCount}</td></tr></table></div></div>'
             }
         ]
     }
