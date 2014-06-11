c3638cea32b5b2f00de13ff934fbb7cc8e35c497
Marcel Atnyashev <atn@marsel.name>
2014-06-11 15:35:47 +0600

    fix

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-        Ext.Viewport.add(Ext.create('InitProMobile.view.Card'));
+        Ext.Viewport.add(Ext.create('InitProMobile.view.Main'));
     },
 
     onUpdated: function() {
