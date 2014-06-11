643f8fbd3b34995a9d8bbb472ad95698e8f57041
root <root@Debian-60-squeeze-64-minimal>
2014-06-11 15:37:13 +0600

    Main.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -11,17 +11,79 @@ Ext.define('InitProMobile.view.Main', {
         items: [
             {
                 xtype: 'list',
-                title: 'Поиск тендеров',
+                title: 'Поиск',
                 //fullscreen: false,
-                itemTpl: '{title}',
-                data: [
-                    { title: 'Item 1' },
-                    { title: 'Item 2' },
-                    { title: 'Item 3' },
-                    { title: 'Item 4' }
-                ]
+                itemTpl: '{tenderid}',
+                store: Ext.create('Ext.data.Store', {
+                    remoteFilter: true,
+                    fields: [
+                        'tenderid', 'name'
+                    ],
+
+                    proxy: {                        
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8'
+                        },
+
+                        actionMethods: {
+                            read : 'POST'
+                        },
+                        api: {
+                            read: '/api/tender/getHash'
+                        },
+
+                        type: "REST",
+
+                        reader: {
+                            type: "json",
+                            rootProperty: "tenders"
+                        }
+                    },
+                    autoLoad: true
+                })
             }
 
         ]
+    }
+});
+
+Ext.define('MyApp.proxy.FileContent', {
+    extend: 'Ext.data.proxy.Proxy',
+    alias: 'proxy.FileContent',
+
+    create: function (operation, callback, scope) {
+             //Here goes your create logic
+        },
+        read: function (operation, callback, scope) {
+            Ext.Ajax.request({
+                url: apiUrl + 'Files/GetFileInfo',
+                method: "POST",
+                defaultHeaders:{
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+                jsonData: '{"file": "myfile"}',
+                success: function(response){
+                    var json = JSON.parse(response.responseText);
+                },
+                failure: function(){                 
+                    alert("fail");
+                }
+            });  
+        },
+        update: function (operation, callback, scope) {
+             //Here goes your update logic
+        },
+        destroy: function (operation, callback, scope) {
+             //Here goes your delete logic
+        }
+});
+
+Ext.define("MyApp.store.FileContent", {
+    extend: "Ext.data.Store",
+    model:'MyApp.model.FileContent',
+    autoLoad: true,
+    requires: ['MyApp.proxy.FileContent'],
+    proxy: {
+        type: 'FileContent'
     }
 });
