784fd42b57004e0933cde3591872be77719605f1
Dima <molot-perm@yandex.ru>
2014-06-11 17:50:34 +0600

    icons

--- app.js
+++ app.js
@@ -23,14 +23,15 @@ Ext.application({
 
     views: [
         'Main',
-        'Card'
+        'Card',
+        'Wellcome'
     ],
 
     icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/new/Icon.png',
+        '72': 'resources/icons/new/Icon~ipad.png',
+        '114': 'resources/icons/new/Icon@2x.png',
+        '144': 'resources/icons/new/Icon~ipad@2x.png'
     },
 
     isIconPrecomposed: true,
@@ -49,7 +50,9 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-        Ext.Viewport.add(Ext.create('InitProMobile.view.Main'));
+        //Ext.Viewport.add(Ext.create('InitProMobile.view.Main'));
+        Ext.Viewport.add(Ext.create('InitProMobile.view.Wellcome'));
+
     },
 
     onUpdated: function() {
