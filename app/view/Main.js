b70910c7e0962aecd20da5b1c7f4f890fd53b4f6
Marcel Atnyashev <atn@marsel.name>
2014-06-11 12:03:28 +0600

    mmm

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,63 +1,59 @@
 Ext.define('InitProMobile.view.Main', {
-    extend: 'Ext.tab.Panel',
     xtype: 'main',
     requires: [
-       // 'Ext.TitleBar',
-       // 'Ext.Video',
-       // 'Ext.dataview.List'
+        'Ext.Container',
+        'Ext.List'
     ],
     config: {
-        tabBarPosition: 'top',
 
         items: [
-            {
-                title: 'tab1',
+
+            Ext.create('Ext.Container', {
+                fullscreen: true,
                 layout: 'fit',
+
                 items: [
-                    Ext.create('Ext.dataview.List', {
-                    fullscreen: true,
-        //            height: 300,
-                    itemTpl: '<div class="contact">{name} <strong>{email}</strong></div>',
-                    store: Ext.create('Ext.data.Store', {
-                        storeId: 'simpsonsStore',
-                        fields: ['name', 'email', 'phone'],
-                        data: [
-                            { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
-                            { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
-                            { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
-                            { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
-                        ]
+                    Ext.create('Ext.List', {
+                        fullscreen: false,
+                        store: {
+                            fields: ['name', 'price', 'finishDocDate'],
+                            data: [{
+                                name: 'Тендер 1',
+                                price: '1231.11 руб.',
+                                finishDocDate: '12 июля 2014'
+                            }, {
+                                name: 'Тендер 2',
+                                price: '1231.11 руб.',
+                                finishDocDate: '12 июля 2014'
+                            }, {
+                                name: 'Тендер 3',
+                                price: '1231.11 руб.',
+                                finishDocDate: '12 июля 2014'
+                            }, {
+                                name: 'Тендер 4',
+                                price: '1231.11 руб.',
+                                finishDocDate: '12 июля 2014'
+                            }]
+                        },
+
+                        itemTpl: '<span class="tenderRow"> <div class="name">{name}</div> <div class="price">{price}</div> <div class="finishDocDate">{finishDocDate}</div> </span>',
+
+                        listeners: {
+                            select: function(view, record) {
+                                Ext.Msg.alert('Тендер', 'Вы переходите в карточку ' + record.get('name'));
+                            }
+                        }
                     }),
-                    items: [{
-                        xtype: 'button',
-                        scrollDock: 'bottom',
-                        docked: 'bottom',
-                        text: 'Load More...'
-                    }]
-                })
-                ]
-            }
-            /*
-            Ext.create('Ext.grid.Grid', {
-                title: 'Simpsons',
-                //store: Ext.data.StoreManager.lookup('simpsonsStore'),
-                store: Ext.create('Ext.data.Store', {
-                    storeId: 'simpsonsStore',
-                    fields: ['name', 'email', 'phone'],
-                    data: {'items': [
-                        { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
-                        { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
-                        { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
-                        { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
-                    ]}
-                }),
-                columns: [
-                    { text: 'Name',  dataIndex: 'name', width: 200},
-                    { text: 'Email', dataIndex: 'email', width: 250},
-                    { text: 'Phone', dataIndex: 'phone', width: 120}
+                    Ext.create('Ext.Container', {
+                        fullscreen: false,
+                        padding: 10,
+                        items: {
+                            xtype: 'button',
+                            text: 'Расширенный поиск',
+                        }
+                    })
                 ]
             })
-            */
         ]
     }
 });
